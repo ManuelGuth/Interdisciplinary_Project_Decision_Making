4fcfeb3bed073c50deec3dbec4e917aa06ef6fc4
manuel <manuel.guth@gmx.net>
2020-05-13 16:40:41 +0200

    implemented maximum expected value baseline

--- models/max_expected_value/maxExpectedValue.py
+++ models/max_expected_value/maxExpectedValue.py
@@ -32,9 +32,31 @@ class MyModel(ccobra.CCobraModel):
 
     def predict(self, item, **kwargs):
         """ Predicts weighted responses to a given problem.
-
+        a task look like this:
+        task: [[Ha, pHa, La, LotShapeA, NumLotA],[[Hb, pHb, Lb, LotShapeB, NumLotB]],[Amb, Corr]]
         """
-        print(item)
+        task = item.task
+        # calculate ExpValue for A
+        ha, pHa, la, _, _ = task[0]
+        expVA = float(ha) * float(pHa) + (1-float(pHa))*float(la)
+
+        # calculate ExpValue for B
+        hb, pHb, lb, _, _ = task[1]
+        expVB = float(hb) * float(pHb) + (1-float(pHb))*float(lb)
+
+        amb, _ = task[2]
+
+        # case where the participant did not know the probabilities for the B option:
+        if amb == '1':
+            if expVA > int(hb)*0.5 + int(lb)*0.5:
+                return 'A'
+            else:
+                return 'B'
+
+        if expVA > expVB:
+            return 'A'
+        else:
+            return 'B'
 
     def adapt(self, item, target, **kwargs):
         """ Trains the model based on a given problem-target combination.
