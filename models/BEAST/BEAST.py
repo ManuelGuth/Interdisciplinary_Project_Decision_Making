67e229e026899a8f916a2d04b5c67cdb57f4cee5
Manuel Guth <manuel.guth@neptun.uni-freiburg.de>
2020-06-06 17:29:20 +0200

    changed prediction to only make one prediction for each task ~25* speedup

--- models/BEAST/BEAST.py
+++ models/BEAST/BEAST.py
@@ -16,13 +16,15 @@ class BEAST(ccobra.CCobraModel):
         supported_response_types = ['single-choice']
         super(BEAST, self).__init__(
             name, supported_domains, supported_response_types)
+        self.trail_cnt = 0
+        self.Prediction = None
 
     def start_participant(self, **kwargs):
         """ Model initialization method. Used to setup the initial state of
         its datastructures, memory, etc.
 
         """
-        print('Start evaluating participant:', kwargs)
+        print('evaluating participant', kwargs)
 
     def pre_train(self, dataset):
         """ Pre-trains the model based on one or more datasets.
@@ -35,15 +37,31 @@ class BEAST(ccobra.CCobraModel):
         a task look like this:
         task: [[Ha, pHa, La, LotShapeA, NumLotA],[[Hb, pHb, Lb, LotShapeB, NumLotB]],[Amb, Corr]]
         """
-        Ha, pHa, La, LotShapeA, LotNumA = item.task[0]
-        Hb, pHb, Lb, LotShapeB, LotNumB = item.task[1]
-        Amb, Corr = item.task[2]
-        Prediction = BEAST_PRED(float(Ha), float(pHa), float(La), LotShapeA, int(LotNumA), float(Hb), float(pHb),
-                                float(Lb), LotShapeB, int(LotNumB), Amb, Corr)
-        mean_B_choice = np.mean(Prediction)
-        if mean_B_choice > 0.5:
+        self.trail_cnt += 1
+        if self.trail_cnt == 1:
+            Ha, pHa, La, LotShapeA, LotNumA = item.task[0]
+            Hb, pHb, Lb, LotShapeB, LotNumB = item.task[1]
+            Amb, Corr = item.task[2]
+            self.Prediction = BEAST_PRED(float(Ha), float(pHa), float(La), LotShapeA, int(LotNumA), float(Hb), float(pHb),
+                                         float(Lb), LotShapeB, int(LotNumB), Amb, Corr)
+        if self.trail_cnt <= 5:
+            prediction = self.Prediction[0][0]
+        elif 5 < self.trail_cnt <= 10:
+            prediction = self.Prediction[0][1]
+        elif 10 < self.trail_cnt <= 15:
+            prediction = self.Prediction[0][2]
+        elif 15 < self.trail_cnt <= 20:
+            prediction = self.Prediction[0][3]
+        elif 20 < self.trail_cnt <= 25:
+            prediction = self.Prediction[0][4]
+
+        if self.trail_cnt >= 25:
+            self.trail_cnt = 0
+
+        if prediction > 0.5:
             return 'B'
-        return 'A'
+        else:
+            return 'A'
 
     def adapt(self, item, target, **kwargs):
         """ Trains the model based on a given problem-target combination.
