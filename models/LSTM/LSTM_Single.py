07462d3c7b3fcb7aff433c4ea7b8996d846f7d31
Manuel <manuel.guth@gmx.net>
2020-08-27 15:02:01 +0200

    Added LSTM Single and updated the data to use

--- models/LSTM/LSTM_Single.py
+++ models/LSTM/LSTM_Single.py
@@ -1,54 +1,43 @@
-import ccobra
 import torch
-import torch.nn as nn
+import ccobra
 import sys
-#sys.path.append('C:/Users/Manuel/Desktop/Interdisciplinary_Project_Decision_Making/models')
 sys.path.append('..')
-
 from DataLoader import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from copy import deepcopy
 import numpy as np
 
 
-class MyModel(ccobra.CCobraModel):
+class LSTM(ccobra.CCobraModel):
     """
     CCOBRA baseline model for the decision making task.
     """
-    def __init__(self, name='FCNN'):
+    def __init__(self, name='LSTM'):
         """ Model constructor.
 
         """
         # set to path if you would like to load a model instead of training. Else False.
-        # self.load = 'runs/FCNN_ep1000_bs750_lr0.001/best_model.pth'
         self.load = False
         self.lr = 0.001
         self.num_epochs = 100
-        self.batch_size = 20
-        name = name + '_ep{}_bs{}_lr{}'.format(self.num_epochs, self.batch_size, self.lr)
+        self.batch_size = 5
+        self.seq_length = 30
+        name = name + '_ep{}_bs{}_sq{}_lr{}'.format(self.num_epochs, self.batch_size, self.seq_length, self.lr)
+
         supported_domains = ['decision-making']
         supported_response_types = ['single-choice']
         # Call the super constructor to fully initialize the model
-        super(MyModel, self).__init__(
+        super(LSTM, self).__init__(
             name, supported_domains, supported_response_types)
 
-        self.FCNN = nn.Sequential(nn.Linear(22, 200),
-                                  nn.ReLU(),
-                                  nn.Dropout(0.15),
-                                  nn.Linear(200, 275),
-                                  nn.ReLU(),
-                                  nn.Dropout(0.15),
-                                  nn.Linear(275, 100),
-                                  nn.ReLU(),
-                                  nn.Dropout(0.15),
-                                  nn.Linear(100, 2),
-                                  nn.Sigmoid()).cuda()
+        self.LSTM = torch.nn.LSTM(22, 2).cuda()
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.SGD(self.LSTM.parameters(), lr=self.lr)
+
         self.prev_answer = [0.0, 0.0]
         self.cnt = 0
-
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = torch.optim.RMSprop(self.FCNN.parameters(), lr=self.lr)
-        self.adapt_optim = torch.optim.RMSprop(self.FCNN.parameters(), lr=self.lr*0.1)
+        self.c0 = None
+        self.h0 = None
 
     def start_participant(self, **kwargs):
         """ Model initialization method. Used to setup the initial state of
@@ -63,23 +52,33 @@ class MyModel(ccobra.CCobraModel):
         """
         if not self.load:
             writer = SummaryWriter('runs/' + self.name)
-            data_loader = DataLoader(dataset, self.batch_size, single=True)
-            data_loader = data_loader.data_loader
-            train_size = int(0.9 * len(data_loader))
-            val_size = len(data_loader) - train_size
-            train_data, val_data = torch.utils.data.random_split(data_loader, [train_size, val_size])
+            data = DataLoader(dataset, 1, single=True)
+            data = data.data_loader
+            new_data = []
+            for i in range(0, len(data), self.seq_length):
+                _data = []
+                _label = []
+                for each in data[i:i+self.seq_length]:
+                    _data.append(each[0].tolist()[0])
+                    _label.append(each[1].tolist()[0])
+                new_data.append([torch.Tensor(_data).cuda(), torch.Tensor(_label).cuda()])
+            data = new_data
 
+            train_size = int(0.9 * len(data))
+            val_size = len(data) - train_size
+            train_data, val_data = torch.utils.data.random_split(data, [train_size, val_size])
             print('Starting training on {} batches of train data with {} batches of validation data.'.format(train_size,
-                                                                                                             val_size))
+                                                                                                            val_size))
             best_loss = np.inf
             best_model = None
             for epoch in range(self.num_epochs):
                 mean_train_loss = 0
                 mean_train_acc = 0
-                self.FCNN.train()
+                self.LSTM.train()
                 for data, label in train_data:
                     self.optimizer.zero_grad()
-                    predictions = self.FCNN(data)
+                    data = torch.unsqueeze(data, dim=1)
+                    predictions = torch.squeeze(self.LSTM(data)[0])
                     loss = self.criterion(predictions, torch.argmax(label, dim=1).long())
                     eq = np.equal(torch.argmax(predictions, dim=1).cpu(), torch.argmax(label, dim=1).cpu())
                     acc = torch.mean(eq.float())
@@ -91,10 +90,11 @@ class MyModel(ccobra.CCobraModel):
                 mean_train_acc = mean_train_acc / train_size
                 mean_val_loss = 0
                 mean_val_acc = 0
-                self.FCNN.eval()
+                self.LSTM.eval()
                 for data, label in val_data:
                     with torch.no_grad():
-                        predictions = self.FCNN(data)
+                        data = torch.unsqueeze(data, dim=1)
+                        predictions = torch.squeeze(self.LSTM(data)[0])
                         loss = self.criterion(predictions, torch.argmax(label, dim=1).long())
                         eq = np.equal(torch.argmax(predictions, dim=1).cpu(), torch.argmax(label, dim=1).cpu())
                         acc = torch.mean(eq.float())
@@ -104,16 +104,17 @@ class MyModel(ccobra.CCobraModel):
                 mean_val_acc = mean_val_acc / val_size
 
                 if mean_val_loss < best_loss:
-                    best_model = deepcopy(self.FCNN)
-                    torch.save(self.FCNN.state_dict(), 'runs/' + self.name + '/best_model.pth')
+                    best_model = deepcopy(self.LSTM)
+                    torch.save(self.LSTM.state_dict(), 'runs/' + self.name + '/best_model.pth')
                     best_loss = mean_val_loss
+                    print('saving at: ', 'runs/' + self.name + '/best_model.pth')
                 writer.add_scalar('Loss/val', mean_val_loss, epoch)
                 writer.add_scalar('Loss/train', mean_train_loss, epoch)
                 writer.add_scalar('Acc/train', mean_train_acc, epoch)
                 writer.add_scalar('Acc/val', mean_val_acc, epoch)
                 print('Epoch {}/{} train loss: {}, train acc: {}, val loss: {}, val acc: {}'.format(epoch,
-                      self.num_epochs-1,mean_train_loss, mean_train_acc, mean_val_loss, mean_val_acc))
-            self.FCNN = best_model
+                      self.num_epochs-1, mean_train_loss, mean_train_acc, mean_val_loss, mean_val_acc))
+            self.LSTM = best_model
         else:
             print('Loading model settings from {}'.format(self.load))
             self.load_model()
@@ -123,41 +124,30 @@ class MyModel(ccobra.CCobraModel):
         a task look like this:
         task: [[Ha, pHa, La, LotShapeA, NumLotA],[[Hb, pHb, Lb, LotShapeB, NumLotB]],[Amb, Corr]]
         """
-        self.FCNN.eval()
+        self.LSTM.eval()
         choice = ['A', 'B']
         data = DataLoader([item, kwargs, self.prev_answer], batch_size=1, eval=True, single=True)
-        data = data.data_loader
+        data = torch.unsqueeze(torch.Tensor(data.data_loader).cuda(), dim=0)
+        data = torch.unsqueeze(data, dim=0)
         with torch.no_grad():
-            predictions = self.FCNN(torch.Tensor(data).cuda())
-            answer = torch.argmax(predictions)
+            self.LSTM.flatten_parameters()
+            if self.h0 is not None:
+                prediction, (self.c0, self.h0) = self.LSTM(data, (self.h0, self.c0))
+            else:
+                prediction, (self.c0, self.h0) = self.LSTM(data)
+            answer = int(torch.argmax(prediction))
             return choice[answer]
 
     def adapt(self, item, target, **kwargs):
         """ Trains the model based on a given problem-target combination.
 
         """
-        # retrain model on person
-        self.FCNN.train()
-        self.optimizer.zero_grad()
-        data = DataLoader([item, kwargs, self.prev_answer], batch_size=1, eval=True, single=True)
-        data = data.data_loader
-        predictions = self.FCNN(torch.Tensor(data).cuda())
-        predictions = torch.unsqueeze(predictions, dim=0)
-        if target[0][0] == 'A':
-            label = torch.Tensor([0]).long().cuda()
-        else:
-            label = torch.Tensor([1]).long().cuda()
-        loss = self.criterion(predictions, label)
-        loss.backward()
-        self.adapt_optim.step()
         if target[0][0] == 'A':
             self.prev_answer = [1.0, 0.0]
         else:
             self.prev_answer = [0.0, 1.0]
-        if self.cnt % 25 == 0 and self.cnt > 0:
-            self.prev_answer = [0.0, 0.0]
         self.cnt += 1
 
     def load_model(self):
         state_dict = torch.load(self.load)
-        self.FCNN.load_state_dict(state_dict)
+        self.LSTM.load_state_dict(state_dict)
